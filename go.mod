1effb0cacdda50b2ae07ae6009e4af77c63c30bc
yfaheid <131709066+yfaheid@users.noreply.github.com>
2026-06-09 14:00:48 -0500

    add producer

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/yfaheid/go-job-queue
 
 go 1.25.0
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/redis/go-redis/v9 v9.20.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
---
+++ producer/producer.go
@@ -0,0 +1,28 @@
+package producer
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+	"github.com/yfaheid/go-job-queue/job"
+)
+
+func Enqueue(rdb *redis.Client, jobType string, payload string) error {
+	j := job.Job{
+		ID:      uuid.NewString(),
+		Type:    jobType,
+		Payload: payload,
+		Status:  "pending",
+	}
+
+	data, err := json.Marshal(j)
+	if err != nil {
+		return fmt.Errorf("failed to marshal job: %w", err)
+	}
+
+	ctx := context.Background()
+	return rdb.LPush(ctx, "jobs", data).Err()
+}
